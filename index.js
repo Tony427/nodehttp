ca3fd115587461b8144d1b1778242148370a2bb4
TonyTsai <ttony427@gmail.com>
2017-10-23 11:34:10 +0800

    first commit with git ignore file

---
+++ index.js
@@ -0,0 +1,25 @@
+var server;
+var port = 2012;
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+server = http.createServer(
+    function(req, res) {
+        path = url.parse(req.url);
+        res.writeHead(200, { 'Content-type': 'text/plain' });
+        switch  (path.pathname){
+            case '/' :
+                res.end('Hello world');
+                break;
+            case '/index' :
+                res.end('index page');
+                break;
+            default :
+                res.end('path.pathname='+path.pathname);
+                break;
+        }
+        //console.log('guest visited');
+    }
+).listen(port);
+console.log('Server is running');
