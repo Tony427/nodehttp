0426ed59a52cf1d8a628590c4dc4d1621785511d
TonyTsai <ttony427@gmail.com>
2017-10-23 11:41:47 +0800

    try commit

--- index.js
+++ index.js
@@ -8,18 +8,19 @@ server = http.createServer(
     function(req, res) {
         path = url.parse(req.url);
         res.writeHead(200, { 'Content-type': 'text/plain' });
-        switch  (path.pathname){
-            case '/' :
+        switch (path.pathname) {
+            case '/':
                 res.end('Hello world');
                 break;
-            case '/index' :
+            case '/index':
                 res.end('index page');
                 break;
-            default :
-                res.end('path.pathname='+path.pathname);
+            default:
+                res.end('path.pathname=' + path.pathname);
                 break;
         }
         //console.log('guest visited');
+        console.log('try rebase');
     }
 ).listen(port);
 console.log('Server is running');
